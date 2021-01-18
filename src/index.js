1a8ad02026b285af896289589f6706454a5ee434
Tim <jingweihu.usc@gmail.com>
2021-01-17 17:39:31 -0800

    First Template Version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'
+import ReduxThunk from 'redux-thunk'
+import axios from 'axios';
+import * as serviceWorker from './serviceWorker';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import config from './config';
+import reducers from './reducers';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+axios.defaults.baseURL = config.baseURLApi;
+axios.defaults.headers.common['Content-Type'] = "application/json";
+//backend token
+const token = localStorage.getItem('token');
+if (token) {
+    axios.defaults.headers.common['Authorization'] = token;
+}
+
+const store = createStore(
+  reducers,
+  applyMiddleware(ReduxThunk)
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
